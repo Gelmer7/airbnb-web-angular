1384eecf168848a9b1ffb8338eeb4efdb17b8b8c
Gelmer Apaza <gelmer.calla@bsource.com.br>
2025-11-05 10:24:21 -0300

    feat: update app theme and enable live reload
    - Change theme from Aura to Material in app config
    - Add live reload option to angular.json
    - Simplify README.md content
    - Add new primary button with check icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import material from '@primeuix/themes/material';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: Aura
+                preset: material
             }
         })
   ]
